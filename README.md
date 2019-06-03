0687e0485e43abc15a9cb9839433c5ae076716fe
Muhammad Junaid <junaid1460@gmail.com>
2019-06-03 20:08:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Simple redis cache
+<span class="badge-npmversion"><a href="https://www.npmjs.com/package/@junaid1460/simple-redis-cache" title="View this project on NPM"><img src="https://img.shields.io/npm/v/@junaid1460/hapi-decorators.svg" alt="NPM version" /></a></span>
+Cache data easily with a simple wrap.
+
+```shell
+
+npm i @junaid1460/hapi-decorators
+```
+
+### Usage
---
+++ src/index.ts
@@ -0,0 +1,88 @@
+import { Request, ResponseToolkit, RouteOptions, ServerRoute } from "hapi";
+import { join } from "path";
+
+interface IRouteSetOptions {
+    baseUrl?: string;
+    auth?: any;
+}
+
+export class HapiServerRoutes {
+    public routes: ServerRoute[];
+}
+
+export function RouteSet(args: IRouteSetOptions) {
+    return function<T extends typeof HapiServerRoutes>(target: T) {
+        const hapiTarget = (target as any) as Function;
+        if (!hapiTarget.prototype.routes) {
+            (hapiTarget as any).routes = [];
+        }
+        target.prototype.routes.forEach((e) => {
+            const path: string[] = ["/"];
+            if (args.baseUrl) {
+                path.push(args.baseUrl);
+            }
+            path.push(e.path);
+            e.path = join(...path, "/");
+            e.options = (e.options || {}) as RouteOptions;
+
+            e.options!.auth = args.auth;
+        });
+        return hapiTarget as any;
+    };
+}
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+type IRouteOptions = Omit<ServerRoute, "path" | "method" | "handler">;
+function Route(
+    method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE" | "OPTIONS",
+) {
+    return function Get(path?: string, args?: IRouteOptions) {
+        return function<T extends HapiServerRoutes>(
+            target: T,
+            propertyKey: string | symbol,
+            descriptor: TypedPropertyDescriptor<
+                (
+                    request: Request,
+                    toolkit: ResponseToolkit,
+                    error?: Error,
+                ) => any
+            >,
+        ) {
+            if (!target.routes) {
+                target.routes = [];
+            }
+            target.routes.push({
+                ...(args || ({} as any)),
+                handler: descriptor.value,
+                path: path || propertyKey,
+                method: method,
+            });
+            return descriptor;
+        };
+    };
+}
+
+export const Get = Route("GET");
+export const Post = Route("POST");
+export abstract class IHapiModule {
+    public routeSets: Array<typeof HapiServerRoutes>;
+    public baseUrl?: string;
+    public auth?: any;
+
+    public getRoutes(): ServerRoute[] {
+        const routes: ServerRoute[] = [];
+        for (const set of this.routeSets) {
+            for (const route of set.prototype.routes) {
+                if (this.baseUrl) {
+                    route.path = join("/", this.baseUrl, route.path);
+                }
+                if (this.auth) {
+                    route.options = (route.options || {}) as RouteOptions;
+                    route.options.auth = this.auth;
+                }
+                routes.push(route);
+            }
+        }
+        return routes;
+    }
+}
